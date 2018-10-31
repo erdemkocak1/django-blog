709eceebde392047410117e497223fc189a0d4cf
DogancanAvc <dogancan.avci@bilgiedu.net>
2018-10-31 18:06:47 +0300

    model ve admin.py geliştirildi

--- uygulama/admin.py
+++ uygulama/admin.py
@@ -3,7 +3,30 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from .models import Blog, Category
+from uygulama.models import Blog, Category
 
-admin.site.register(Blog)
-admin.site.register(Category)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'timeread','dateet' ,'durum')
+
+    fields = ('title', 'slug', 'category', 'body', 'author','durum', 'timeread')
+
+    list_filter = ('title', 'category')
+    list_display_links = ('title','category')
+    ordering = ('author', 'title')
+    view_on_site = True
+    search_fields = ['title', 'author']
+    # list_max_show_all = 10
+    list_per_page = 5
+
+
+
+
+admin.site.register(Blog, BlogAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'durum')
+    fields = ('title', 'durum')
+
+admin.site.register(Category, CategoryAdmin)
--- uygulama/models.py
+++ uygulama/models.py
@@ -7,22 +7,22 @@ from django.db import models
 
 #
 class Category(models.Model):
-    title = models.CharField(max_length=100, unique=True, help_text="Başlık buraya yazılacak")
+    title = models.CharField('Kategori', max_length=100, unique=True, help_text="Kategori İsmi Yazılacak")
+    durum = models.BooleanField('Erişime Açık', default=True)
 
-
-#     slug = models.SlugField(max_length=100, db_index=True)
-#     durum           = models.BooleanField(_('Durum'), default=True)
-#
-# def __str__(self):
-#     return '%s' %  self.title
-#
-# class Meta:
-#     verbose_name_plural         =           ('Categorys')
-#     verbose_name                =           ('Category')
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100, unique=True,help_text="Başlık buraya yazılacak")
-    slug = models.SlugField(max_length=100, unique=True)
-    body = models.TextField(help_text="Makale buraya yazılacak")
-    posted = models.DateField(db_index=True, auto_now_add=True)
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name=('Kategori'))
+    onemin = '1'
+    twomin = '2'
+    threemin = '3'
+    fourmin = '4'
+    CHOICES = ((onemin, '1dk'), (twomin, '2dk'), (threemin, '3dk'), (fourmin, '4dk'),)
+
+    title = models.CharField('Başlık', max_length=100, unique=True, blank=False, null=False, default='', help_text="Başlık buraya yazılacak")
+    slug = models.SlugField(max_length=100, unique=True, null=True)
+    body = models.TextField('İçerik', help_text="Makale buraya yazılacak", blank=False, null=False, default='')
+    author = models.CharField('Yazar', max_length=20, help_text="Yazar", null=False, default='')
+    category = models.CharField('Kategori', max_length=10, blank=False, null=False, default='')
+    durum = models.BooleanField('Erişim', default=True)
+    dateet = models.DateTimeField(auto_now=True)
+    timeread = models.CharField('Okuma Süresi', max_length=2, choices=CHOICES, default=2)
