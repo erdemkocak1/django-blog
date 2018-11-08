9fda2b3bb80a84294621f13bb4c8d3fe9c9cc9f1
erdem <erdem.kocak@makdos.com>
2018-11-08 12:47:13 +0300

    sunucu ayarları

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -6,12 +6,11 @@ from uygulama import views
 
 urlpatterns = [
 
-    path('cat/<slug:slug>/', views.categorysec),        # cat/ şeklinde gelirse view.py dosyasındaki categorysec kısmı çalışacak ve categorysec kısmı category.html yardımıyla sonuçları sayfaya yazacak.
 
     path('', views.blog),                          # blog/ şeklinde bir arama gelirse view.py dosyasındaki blog kısmı çalışacak blog kısmı ulaştığı sonuçları main.html yardımıyla sayfaya yazacak.
+    path('cat/<slug:slug>/', views.categorysec),  # cat/ şeklinde gelirse view.py dosyasındaki categorysec kısmı çalışacak ve categorysec kısmı category.html yardımıyla sonuçları sayfaya yazacak.
+
     path('blog/detay/<slug:slug>/', views.blogdetay),   # blog/detay gibi bir arama gelirse veya makalelerin içeriğine girilmek istenirse burası view.py dosyasındaki blogdetay kısmını çalıştıracak ve sonuçları blog_deay.html yardımıyla sayfaya yazacak.
-
-
 
     path('admin/', admin.site.urls),
 
