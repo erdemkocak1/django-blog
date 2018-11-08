505109fd8994b6af38b86d315d044f94f1a549f5
erdem <erdem.kocak@makdos.com>
2018-11-08 12:23:38 +0300

    static ayarları yapıldı.

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -122,8 +122,6 @@ USE_L10N = False
 
 USE_TZ = False
 
+STATIC_URL                                          = '/static/'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_ROOT                                         = os.path.join(BASE_DIR, 'static')              ##### Sunucu için
